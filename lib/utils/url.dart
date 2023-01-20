49284cf03e26832e8d775e0628b94d07741d6f16
davidsolisdev <davidsolisdev@gmail.com>
2023-01-20 09:07:35 -0600

    v1

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,4 +1,11 @@
 String url = "192.168.0.230:8081";
 
-Uri URL(String? params, Map<String, dynamic>? query) =>
-    Uri.http(url, params ?? "", query);
+Uri URL(String? params, Map<String, dynamic>? query) {
+  if (query != null) {
+    List<String> keys = query.keys.toList();
+    for (var i = 0; i < keys.length; i++) {
+      if (query[keys[i]] == null) query.remove(keys[i]);
+    }
+  }
+  return Uri.http(url, params ?? "", query);
+}
