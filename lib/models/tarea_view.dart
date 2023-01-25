b90f36e801095ed5cff856fc30c33ee1a5a10004
davidsolisdev <davidsolisdev@gmail.com>
2023-01-25 09:25:46 -0600

    v1

--- lib/models/tarea_view.dart
+++ lib/models/tarea_view.dart
@@ -21,13 +21,13 @@ class TareaView {
 
   factory TareaView.fromJson(Map<String, dynamic> json) => TareaView(
         empresa: json["empresa"],
-        noTarea: json["noTarea"],
-        tipoDeTarea: json["tipoDeTarea"],
+        noTarea: int.tryParse(json["noTarea"])!,
+        tipoDeTarea: int.tryParse(json["tipoDeTarea"])!,
         usuarioCreo: json["usuarioCreo"],
         descripcionDeTarea: json["descripcionDeTarea"],
-        tiempoEstimadoEnMinutos: json["tiempoEstimadoEnMinutos"],
+        tiempoEstimadoEnMinutos: int.tryParse(json["tiempoEstimadoEnMinutos"])!,
         responsable: json["responsable"],
-        estadoTarea: json["estadoTarea"],
+        estadoTarea: int.tryParse(json["estadoTarea"])!,
       );
 
   static List<TareaView> fromJsonList(List<Map<String, dynamic>> lista) =>
