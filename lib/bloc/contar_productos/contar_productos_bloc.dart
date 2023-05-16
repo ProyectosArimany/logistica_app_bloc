94b10291f10ecdd8492def07436cb870bc3f188b
davidsolisdev <davidsolisdev@gmail.com>
2023-05-16 09:54:41 -0600

    v1

--- lib/bloc/contar_productos/contar_productos_bloc.dart
+++ lib/bloc/contar_productos/contar_productos_bloc.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:logistica_app_bloc/models/almacen.dart';
 import 'package:logistica_app_bloc/models/params/get_nombre_producto_query.dart';
 import 'package:logistica_app_bloc/models/qr_barcode_data.dart';
+import 'package:logistica_app_bloc/repositories/conteos_repository.dart';
 import 'package:logistica_app_bloc/repositories/productos_repository.dart';
 import 'package:meta/meta.dart';
 
@@ -12,8 +13,10 @@ part 'contar_productos_state.dart';
 class ContarProductosBloc
     extends Bloc<ContarProductosEvent, ContarProductosState> {
   final ProductosRepository productosRepository;
+  final ConteosRepository conteosRespository;
 
-  ContarProductosBloc({required this.productosRepository})
+  ContarProductosBloc(
+      {required this.productosRepository, required this.conteosRespository})
       : super(ContarProductosInitialState()) {
     on<ScanQRDoneEvent>((event, emit) async {
       final resultName = await productosRepository.getNombre(event.token,
